d3270b0fe04a5e2fed50958b7fde34440cf8e54c
Wesley Wang <20538125@student.curtin.edu.au>
2025-02-11 20:20:38 +0800

    Created file and made an API call to check the response

---
+++ hn_data_visualizer.py
@@ -0,0 +1,6 @@
+import requests
+
+# Make an API call and check the response
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
